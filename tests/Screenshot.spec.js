63c1065a87bfda44f7bb5784088d8ea38c0a09e6
Jerin72 <jerinvarghese72@gmail.com>
2026-09-23 20:42:40 +0530

    Screenshot program

---
+++ tests/Screenshot.spec.js
@@ -0,0 +1,15 @@
+import test, { expect } from "@playwright/test";
+
+test("Screenshot", async ({page})=>{
+    await page.goto("https://selenium.qabible.in/")
+    await page.screenshot({path:"home.png"})
+    const inputform=page.getByRole('link',{name:"Input Form"})
+    await inputform.screenshot({path:"input.png"})
+})
+
+test.only("Visual Comparision", async ({page})=>{
+    await page.goto("https://selenium.qabible.in/")
+    //await page.goto("https://demoqa.com/upload-download")
+    await expect(page).toHaveScreenshot("screen2.png")   //first time it will fail but at the same time it will take a screenshot
+
+})
